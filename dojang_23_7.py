b02624c220754092a449e15b3bae4cfcc3b24ac9
MunGyuJang <112140135+MunGyuJang@users.noreply.github.com>
2022-08-27 16:19:54 +0900

    Update dojang_23_7.py

--- dojang_23_7.py
+++ dojang_23_7.py
@@ -12,18 +12,20 @@ for i in range(len(matrix)):        # 세로열 만큼 반복
       temp = [matrix[k] for k in range(i-1, i+2) if k >= 0 and k < row]
       # temp라는 임시 리스트에 세로열 인덱스의 -1 ~ +1을 불러오면서(range(i-1, i+2)) 인덱스가 음수 혹은 세로열을 초과했는지 검색(if k >= 0 and k < row)
       # temp = []
-      # for k in range(i-1, i+2):
-      #   if k >= 0 and k < row:
-      #     temp.append(matrix[k])
+      # for k in range(i-1, i+2):   -- 세로로 한 칸 주변에 있는지
+      #   if k >= 0 and k < row:    -- 혹시 세로열 인덱스가 범위를 벗어나진 않았는지
+      #     temp.append(matrix[k])  -- temp에 저장
 
       temp = [v[k] for k in range(j-1, j+2) if k >= 0 and k < col for v in temp]
       # 주변 세로열을 가져온 temp에 담긴 각 리스트들을(for v in temp) 가로열 인덱스의 -1 ~ +1을 불러오면서(range(j-1, j+2)) 인덱스가 음수 혹은 가로열을 초과했는지 검색(if k >= 0 and k < col)하면서
       # 기존 2차원 리스트가 쪼개지도록 대괄호 생략
-      # for v in temp:
-      #   for k in range(j-1, j+2):
-      #     if k >= 0 and k < col:
-      #       temp.append(v[k]) -- temp 리스트에 이미 다른 요소가 있어서 실제 코드로 사용하려면 새로운 빈 리스트에 할당해야함
+      # for v in temp:                -- temp의 세로열 만큼 반복
+      #   for k in range(j-1, j+2):   -- 가로로 한 칸 주변에 있는지
+      #     if k >= 0 and k < col:    -- 혹시 가로열 인덱스가 범위를 벗어나진 않았는지
+      #       temp.append(v[k])       -- temp 리스트에 이미 다른 요소가 있어서 실제 코드로 사용하려면 새로운 빈 리스트에 할당해야함
 
-      matrix[i][j] = str(temp.count('*')) # temp에서 별의 총 갯수를 세고(temp.count('*') 리스트를 간단하게 합칠 수 있게 join을 쓰기 전 class를 str으로 변환 후 각 '.'였던 요소에 대입
+      # 각 요소마다 상하좌우로 1칸 근처에 있는 요소들을 저장한 temp라는 리스트가 만들어짐
+
+      matrix[i][j] = str(temp.count('*')) # temp에서 별의 총 갯수를 세고(temp.count('*') 리스트를 간단하게 합칠 수 있게 join을 쓰기 전 class를 str으로 변환 후 '.'였던 요소에 대입
       
-  print(''.join(matrix[i]))   # matrix 리스트를 세로줄 별로(들여쓰기를 가로열 for문 종료에 맞춰주기) join 후 출력
+  print(''.join(matrix[i]))   # matrix 리스트를 세로줄 별로(들여쓰기를 가로열 for문 종료에 맞춰줌) join 후 출력
